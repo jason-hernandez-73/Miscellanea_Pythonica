0f38c417b9f56a373af4cd2b03b9aafd7c46d472
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-01-15 14:39:37 -0800

    Update PyPoll.py

--- Python Challenge/PyPoll.py
+++ Python Challenge/PyPoll.py
@@ -18,6 +18,8 @@ print(f'Total Votes: {total_votes}')
 print("-") * 20
 
 # List candidates who received votes
+name = str(PyPoll_Resources_election_data[2])
+
 # function to get unique values 
 def unique(csvfile): 
   
@@ -25,10 +27,10 @@ def unique(csvfile):
     unique_names = [] 
       
     # check for all elements in "Candidate" column
-    for x in csvfile: 
+    for name in csvfile: 
         # check if exists in unique_list or not 
-        if x not in unique_names: 
-            unique_names.append(x) 
+        if name not in unique_names: 
+            unique_names.append(name) 
 
 # Calculate number of votes per candidate
 with open(PyPoll_Resources_election_data) as f:
@@ -37,13 +39,13 @@ with open(PyPoll_Resources_election_data) as f:
     percent_votes = (vote_counts / total_votes)
 
 # Print each candidate's name and number of votes
-print(f {unique_names},": ", {percent_votes}, {vote_counts})
+print(f'{unique_names},": ", {percent_votes}, {vote_counts}')
 
 # Find candidate with maximum number of votes
 winner = max(vote_counts)
 
 # Print winning candiate's name
 print("-") * 20
-if row[0] == 
-print("Winner: ", row[2])
+if row[2] == winner
+print(f"Winner: , {name}, row[2], votes")
 print("-") * 20
