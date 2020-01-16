a1e3c3e37f2411a890cbc0b6694a4c6f22ce5aef
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-01-15 18:32:12 -0800

    Possibly final sumission?

---
+++ Python_Challenge/PyBank.py
@@ -0,0 +1,59 @@
+# Import the file and get past the headers
+import os
+import csv
+budget_data_csv = os.path.join("..", "PyBank_Resources_budget_data.csv")
+
+# Set initial values
+greatest_month_increase = 0
+greatrest_month_decrease = 0
+total_months = 0
+total_profits_losses = 0
+greatest_increase = 0
+greatest_decrease = 0
+
+with open(budget_data_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    budget_header = next(csvfile)
+    
+    # Count the number of months
+    row = next(csvreader)
+    for row in csvreader:
+        previous_row = int(row[1])
+        total_months = total_months + 1
+
+    # Ticker the profits and losses
+        total_profits_losses = total_profits_losses + int(row[1])
+
+# Average the changes in profits and losses
+    change = next(csvreader)
+    for row in csvreader:
+        change = int(row[1]) - previous_row
+    average_change = total_profits_losses / total_months
+
+# Print the number of months
+print(f'Total Months: {total_months}')
+
+# Print the net profits/losses
+print(f'Total: ${total_profits_losses}')
+
+# Print the average change
+print(f'Average Change: {average_change}')
+
+# Find the maximum and minimum change
+greatest_increase = max(change)
+greatest_decrease = min(change)
+
+# Print the maximum and minimum change
+print(f'Greatest Increase in Profits: {greatest_increase}')
+print(f'Greatest Decrease in Profits: {greatest_decrease}')
+
+# Export results to text file
+f = open("profitLoss.txt","w+")
+file.write(
+    f'Total Months: {total_months}', 
+    f'Total: ${total_profits_losses}', 
+    f'Average Change: {average_change}', 
+    f'Greatest Increase in Profits: {greatest_increase}', 
+    f'Greatest Decrease in Profits: {greatest_decrease}'
+)
+f.close()
--- Python_Challenge/PyPoll.py
+++ Python_Challenge/PyPoll.py
@@ -3,39 +3,39 @@ import os
 import csv
 election_data_csv = os.path.join("..", "PyPoll_Resources_election_data.csv")
 
-with open(PyPoll_Resources_election_data) as csvfile:
+with open(election_data_csv) as csvfile:
     csvreader = csv.reader(csvfile)
     headers = next(csvreader, None)
     headers['Voter ID', 'County', 'Candidate']
 
-#Tally total votes cast
-total_votes = sum(1 for row in csvfile)
+    #Tally total votes cast
+    total_votes = sum(1 for row in csvfile)
 
-# Print total votes cast
-print("Election Results")
-print("-") * 20
-print(f'Total Votes: {total_votes}')
-print("-") * 20
+    # Print total votes cast
+    print("Election Results")
+    print("-") * 20
+    print(f'Total Votes: {total_votes}')
+    print("-") * 20
 
-# List candidates who received votes
-name = str(PyPoll_Resources_election_data[2])
+    # List candidates who received votes
+    name = str(election_data_csv[2])
 
-# function to get unique values 
-def unique(csvfile): 
+    # function to get unique values 
+    def unique(csvfile): 
   
-    # intilize a null list 
-    unique_names = [] 
+       # intilize a null list 
+       unique_names = [] 
       
-    # check for all elements in "Candidate" column
+        # check for all elements in "Candidate" column
     for name in csvfile: 
         # check if exists in unique_list or not 
         if name not in unique_names: 
             unique_names.append(name) 
 
 # Calculate number of votes per candidate
-with open(PyPoll_Resources_election_data) as f:
+with open(election_data_csv) as f:
     reader = csv.reader(f)
-    vote_counts = Counter(map(itemgetter(2), reader))
+    vote_counts = Counter(map(operator.itemgetter(2), reader))
     percent_votes = (vote_counts / total_votes)
 
 # Print each candidate's name and number of votes
@@ -46,6 +46,6 @@ winner = max(vote_counts)
 
 # Print winning candiate's name
 print("-") * 20
-if row[2] == winner
-print(f"Winner: , {name}, row[2], votes")
-print("-") * 20
+if row[2] == winner:
+    print(f"Winner: , {name}, row[2], votes")
+    print("-") * 20
