8d8af8ca803f2dba84d81aa77f0ada2f27b60f0e
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-02-10 14:22:05 -0800

    Begin API work

--- API-Challenge/api_keys.py
+++ API-Challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "b69894b6d1b0d5d326c5d9f1721b6e26"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAUk8DDaxa6zVUDtu6TjvXJ_cR5-RVyOYI"
