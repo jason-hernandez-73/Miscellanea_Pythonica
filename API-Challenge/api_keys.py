4ba0e90f282584f4aaf4821666ebb18078b43fb2
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-02-18 13:07:26 -0800

    Hiding api keys

--- API-Challenge/api_keys.py
+++ API-Challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "b69894b6d1b0d5d326c5d9f1721b6e26"
+weather_api_key = "xxxxxxxxxxxxxxxxxx"
 
 # Google API Key
-g_key = "AIzaSyAUk8DDaxa6zVUDtu6TjvXJ_cR5-RVyOYI"
+g_key = "xxxxxxxxxxxxxxxxxx"
