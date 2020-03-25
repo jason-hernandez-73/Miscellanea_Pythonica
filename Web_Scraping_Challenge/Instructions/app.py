364452d524f2cb633701081c6b2a751ece7a6a0e
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-03-24 17:11:38 -0700

    Initial code files

--- Web_Scraping_Challenge/Instructions/app.py
+++ Web_Scraping_Challenge/Instructions/app.py
@@ -6,8 +6,10 @@ app=Flask(__name__)
 
 # @TODO:  Create a route and view function that takes in a string and renders index.html template
 @app.route("/")
-def echo():
-    return render_template("index.html", text="Jason Hernandez", hobby="Gardening")
+def scraper():
+    news_title =
+    news_p =
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
