80642445cec3385344bb28b605dbc53ed3520a68
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-03-26 14:37:13 -0700

    a bit from the video

--- Web_Scraping_Challenge/Instructions/app.py
+++ Web_Scraping_Challenge/Instructions/app.py
@@ -1,27 +1,31 @@
 # import necessary libraries
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import # other code file
+import mission_to_mars.ipynb
 
 # @TODO: Initialize your Flask app here
 app=Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_to_mars")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/missions_to_mars")
 
 # @TODO:  Create a route and view function that takes in a string and renders index.html template
 @app.route("/")
 def home():
-
+    missions_to_mars.mars_news.insert_one(
+        {
+            'news': news_title,
+            'paragraph': news_p
+        }
+    )
     # Find one record of data from the mongo database
     # variable = mongo.db. # collection .find_one()
 
     # Return template and data
-    return render_template("index.html", # variable = from above)
+    return render_template("index.html", news_title, news_p)
 
 @app.route("/scrape")
 def scrape():
-    news_title =
-    news_p =
+    
     return render_template("index.html")
 
     redirect("/")
